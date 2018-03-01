b7ed0d82880201d22fefcab197d29d0a0b6449de
Daniel Hasselwander <daniel.hasselwander@nttdata.com>
2018-03-01 22:21:07 +0100

    Comment Beans

--- src/main/java/beans/AnzeigeBean.java
+++ src/main/java/beans/AnzeigeBean.java
@@ -15,40 +15,41 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-
 @Stateless
-public class AnzeigeBean  extends EntityBean<Anzeige, Long> { 
-   
+public class AnzeigeBean extends EntityBean<Anzeige, Long> {
+
     public AnzeigeBean() {
         super(Anzeige.class);
     }
-    
+
     /**
      * Alle Aufgaben eines Benutzers, nach Fälligkeit sortiert zurückliefern.
-     * @param username Benutzername
+     *
+     * @param benutzername Benutzername
      * @return Alle Aufgaben des Benutzers
      */
     public List<Anzeige> findByUsername(String benutzername) {
         return em.createQuery("SELECT t FROM Anzeige t WHERE t.benutzer.benutzername = :benutzername ORDER BY t.einstellungsdatum")
-                 .setParameter("benutzername", benutzername)
-                 .getResultList();
+                .setParameter("benutzername", benutzername)
+                .getResultList();
     }
-    
+
     /**
      * Suche nach Aufgaben anhand ihrer Bezeichnung, Kategorie und Status.
-     * 
-     * Anders als in der Vorlesung behandelt, wird die SELECT-Anfrage hier
-     * mit der CriteriaBuilder-API vollkommen dynamisch erzeugt.
-     * 
+     *
+     * Anders als in der Vorlesung behandelt, wird die SELECT-Anfrage hier mit
+     * der CriteriaBuilder-API vollkommen dynamisch erzeugt.
+     *
      * @param search In der Kurzbeschreibung enthaltener Text (optional)
-     * @param category Kategorie (optional)
-     * @param status Status (optional)
+     * @param kategorie Kategorie (optional)
+     * @param art ArtDerAnzeige (optional)
+     * @param artDesPreises ArtDesPreises (optional)
      * @return Liste mit den gefundenen Aufgaben
      */
     public List<Anzeige> search(String search, Kategorie kategorie, ArtDerAnzeige art, ArtDesPreises artDesPreises) {
         // Hilfsobjekt zum Bauen des Query
         CriteriaBuilder cb = this.em.getCriteriaBuilder();
-        
+
         // SELECT t FROM Task t
         CriteriaQuery<Anzeige> query = cb.createQuery(Anzeige.class);
         Root<Anzeige> from = query.from(Anzeige.class);
@@ -56,29 +57,28 @@ public class AnzeigeBean  extends EntityBean<Anzeige, Long> {
 
         // ORDER BY dueDate, dueTime
         query.orderBy(cb.asc(from.get("einstellungsdatum")), cb.asc(from.get("einstellungszeit")));
-        
+
         // WHERE t.shortText LIKE :search
         if (search != null && !search.trim().isEmpty()) {
             query.where(cb.like(from.get("titel"), "%" + search + "%"));
         }
-        
+
         // WHERE t.category = :category
         if (kategorie != null) {
             query.where(cb.equal(from.get("kategorie"), kategorie));
         }
-        
+
         // WHERE t.status = :status
         if (art != null) {
             query.where(cb.equal(from.get("art"), art));
         }
-        
-                // WHERE t.status = :status
+
+        // WHERE t.status = :status
         if (artDesPreises != null) {
             query.where(cb.equal(from.get("artDesPreises"), artDesPreises));
         }
-        
+
         return em.createQuery(query).getResultList();
     }
-  
-    
+
 }
--- src/main/java/beans/BenutzerBean.java
+++ src/main/java/beans/BenutzerBean.java
@@ -42,11 +42,25 @@ public class BenutzerBean extends EntityBean<Benutzer, String> {
     public Benutzer getCurrentUser() {
         return this.em.find(Benutzer.class, this.ctx.getCallerPrincipal().getName());
     }
-
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="Methoden zum Anlegen von Usern">
+    
     /**
+     * Speichert User in Usertabelle und fügt den anglegten User der Gruppe zu
+     * Wenn Benutzername schon existiert wirft er Exeption
      *
-     * @param username
+     * @param benutzername
      * @param password
+     * @param vorname
+     * @param nachname
+     * @param strasse
+     * @param hausnummer
+     * @param telefonnummer
+     * @param postleitzahl
+     * @param land
+     * @param ort
+     * @param eMail
      * @throws BenutzerBean.UserAlreadyExistsException
      */
     public void signup(String benutzername, String password, String vorname, String nachname, String strasse, String hausnummer, String postleitzahl, String ort, String land, String eMail, String telefonnummer) throws UserAlreadyExistsException {
@@ -58,15 +72,37 @@ public class BenutzerBean extends EntityBean<Benutzer, String> {
         user.addToGroup("youbuy-app-user");
         em.persist(user);
     }
-    
-        public void signup(Benutzer user) throws UserAlreadyExistsException {
+    /** Speichert User in Usertabelle und fügt den anglegten User der Gruppe zu
+     * Wenn Benutzername schon existiert wirft er Exeption
+     * @param user
+     * @throws BenutzerBean.UserAlreadyExistsException
+     * 
+     */
+    public void signup(Benutzer user) throws UserAlreadyExistsException {
         if (em.find(Benutzer.class, user.getBenutzername()) != null) {
             throw new UserAlreadyExistsException("Der Benutzername $B ist bereits vergeben.".replace("$B", user.getBenutzername()));
         }
         user.addToGroup("youbuy-app-user");
         em.persist(user);
     }
-
+    //</editor-fold>
+   
+    /**
+     * Verändert den Bentuzer ohne zu persistieren
+     * @param benutzername
+     * @param password
+     * @param oldPassword
+     * @param vorname
+     * @param nachname
+     * @param strasse
+     * @param hausnummer
+     * @param postleitzahl
+     * @param ort
+     * @param land
+     * @param eMail
+     * @param telefonnummer
+     * @throws InvalidCredentialsException
+     */
     public void changeData(String benutzername, String password, String oldPassword, String vorname, String nachname, String strasse, String hausnummer, String postleitzahl, String ort, String land, String eMail, String telefonnummer) throws InvalidCredentialsException {
 
         Benutzer user = findById(benutzername);
@@ -95,14 +131,13 @@ public class BenutzerBean extends EntityBean<Benutzer, String> {
      * @param user
      * @param oldPassword
      * @param newPassword
-     * @throws UserBean.InvalidCredentialsException
+     * @throws beans.BenutzerBean.InvalidCredentialsException
      */
     @RolesAllowed("youbuy-app-user")
     public void changePassword(Benutzer user, String oldPassword, String newPassword) throws InvalidCredentialsException {
         if (user == null || !user.checkPassword(oldPassword)) {
             throw new InvalidCredentialsException("Benutzername oder Passwort sind falsch.");
         }
-
         user.setPassword(newPassword);
     }
 
@@ -112,6 +147,7 @@ public class BenutzerBean extends EntityBean<Benutzer, String> {
      * @param user Zu löschender Benutzer
      */
     @RolesAllowed("youbuy-app-user")
+    @Override
     public void delete(Benutzer user) {
         this.em.remove(user);
     }
@@ -123,15 +159,17 @@ public class BenutzerBean extends EntityBean<Benutzer, String> {
      * @return Gespeicherter Benutzer
      */
     @RolesAllowed("youbuy-app-user")
+    @Override
     public Benutzer update(Benutzer user) {
         return em.merge(user);
     }
 
+   //<editor-fold defaultstate="collapsed" desc="Eigene Exceptions">
+    
     /**
      * Fehler: Der Benutzername ist bereits vergeben
      */
     public class UserAlreadyExistsException extends Exception {
-
         public UserAlreadyExistsException(String message) {
             super(message);
         }
@@ -142,10 +180,9 @@ public class BenutzerBean extends EntityBean<Benutzer, String> {
      * überein
      */
     public class InvalidCredentialsException extends Exception {
-
         public InvalidCredentialsException(String message) {
             super(message);
         }
     }
-
+    //</editor-fold>
 }
